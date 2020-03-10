ecd250983cc443fa29a009fb0e2763bb0176b4a7
valentincraciun99 <valentin.craciun99@gmail.com>
2020-03-10 23:33:03 +0200

    Add elimination without partial pivoting

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import numpy as np
+from numpy import array, zeros, fabs
+
 
 def elimination(matrix):
-    for k in range(n-2):
-        factor=a[k][k]
+    for k in range(n - 2):
+        factor = a[k][k]
 
-a = np.array([[2, 1, 3],
-              [3, 2, 7],
-              [1, 7, 2]],
-             float)
-n = 3
 
-b = np.array([1, 2, 3],
-             float)
-x= np.array([0,0,0])
+a = array([[2, 1, 3],
+           [3, 2, 7],
+           [1, 7, 2]],
+          float)
+b = array([5, 7, 2], float)
+n = len(b)
+x = zeros(n, float)
 
-print(elimination(a))
+
+print(a[1, 1])
+print(b)
+
+# Elimination
+for k in range(n):
+    for i in range(k + 1, n):
+        if a[i,k] == 0:
+            continue
+        factor = a[k,k] / a[i,k]
+        for j in range(k, n):
+            a[i,j] = a[k,j] - a[i,j] * factor
+            b[i] = b[k] - factor * b[i]
+
+print(a)
+print(b)
+
+# Elimination
 
 
