04c37f3c1d7c88b872b034c1beb878c4b3057495
valentincraciun99 <valentin.craciun99@gmail.com>
2020-03-10 23:09:51 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def elimination(matrix):
+    for k in range(n-2):
+        factor=a[k][k]
+
+a = np.array([[2, 1, 3],
+              [3, 2, 7],
+              [1, 7, 2]],
+             float)
+n = 3
+
+b = np.array([1, 2, 3],
+             float)
+x= np.array([0,0,0])
+
+print(elimination(a))
+
+
